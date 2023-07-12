4d9f8bfbbfbc1c9cdf9cdd37eeb1d7d2624879da
ks <294513634@qq.com>
2023-07-12 16:54:35 +0800

    增加用户非法操作校验

--- blog-server/src/main/java/com/iksling/blog/advice/ExceptionAdvice.java
+++ blog-server/src/main/java/com/iksling/blog/advice/ExceptionAdvice.java
@@ -1,24 +1,50 @@
 package com.iksling.blog.advice;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.iksling.blog.constant.CommonConst;
+import com.iksling.blog.entity.UserAuth;
 import com.iksling.blog.exception.FileStatusException;
 import com.iksling.blog.exception.IllegalRequestException;
 import com.iksling.blog.exception.OperationStatusException;
+import com.iksling.blog.mapper.UserAuthMapper;
 import com.iksling.blog.pojo.Result;
+import com.iksling.blog.util.UserUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import static com.iksling.blog.constant.RedisConst.USER_ILLEGAL_OPERATION;
 import static com.iksling.blog.constant.StatusConst.*;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private UserAuthMapper userAuthMapper;
+
     /********** 非法请求异常 **********/
     @ExceptionHandler(value = IllegalRequestException.class)
     public Result exceptionAdvice(IllegalRequestException e) {
+        Integer userId = UserUtil.getLoginUser().getUserId();
+        String username = UserUtil.getLoginUser().getUsername();
+        if (!redisTemplate.hasKey(USER_ILLEGAL_OPERATION))
+            redisTemplate.boundHashOps(USER_ILLEGAL_OPERATION).expire(1, TimeUnit.DAYS);
+        redisTemplate.boundHashOps(USER_ILLEGAL_OPERATION).increment(userId, 1);
+        Integer count = (Integer) redisTemplate.boundHashOps(USER_ILLEGAL_OPERATION).get(userId);
+        if (count != null && count == 3) {
+            userAuthMapper.update(null, new LambdaUpdateWrapper<UserAuth>()
+                    .set(UserAuth::getLockedFlag, true)
+                    .set(UserAuth::getDisabledFlag, true)
+                    .eq(UserAuth::getUserId, userId));
+            return Result.failure().code(ACCOUNT_LOCKED).message("账户[" + username + "]已被锁定, 如有疑问请联系管理员[" + CommonConst.CONTACT + "]");
+        }
         return Result.failure().code(ILLEGAL_REQUEST).message(e.getMessage());
     }
 
--- blog-server/src/main/java/com/iksling/blog/handler/AuthenticationFailHandlerImpl.java
+++ blog-server/src/main/java/com/iksling/blog/handler/AuthenticationFailHandlerImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.iksling.blog.constant.CommonConst;
 import com.iksling.blog.constant.StatusConst;
 import com.iksling.blog.pojo.Result;
+import com.iksling.blog.util.UserUtil;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
@@ -21,12 +22,13 @@ public class AuthenticationFailHandlerImpl implements AuthenticationFailureHandl
         httpServletResponse.setContentType("application/json;charset=UTF-8");
         int code = StatusConst.AUTHENTICATION_FAILURE;
         String message = e.getMessage();
+        String username = UserUtil.getLoginUser().getUsername();
         if (e instanceof LockedException) {
             code = StatusConst.ACCOUNT_LOCKED;
-            message = "账户被锁定, 如有疑问请联系管理员[" + CommonConst.CONTACT + "]";
+            message = "账户[" + username + "]已被锁定, 如有疑问请联系管理员[" + CommonConst.CONTACT + "]";
         } else if (e instanceof DisabledException) {
             code = StatusConst.ACCOUNT_DISABLED;
-            message = "账户被禁用, 如有疑问请联系管理员[" + CommonConst.CONTACT + "]";
+            message = "账户[" + username + "]已被禁用, 如有疑问请联系管理员[" + CommonConst.CONTACT + "]";
         }
         httpServletResponse.getWriter().write(JSON.toJSONString(Result.failure().code(code).message(message)));
     }

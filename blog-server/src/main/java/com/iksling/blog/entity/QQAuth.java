b1913fa10277d23b89000f0615646b51aced3003
ks <294513634@qq.com>
2023-09-12 18:21:47 +0800

    QQAuth.java 你在干什么

---
+++ blog-server/src/main/java/com/iksling/blog/entity/QQAuth.java
@@ -0,0 +1,82 @@
+package com.iksling.blog.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 
+ * @TableName tb_qq_auth
+ */
+@TableName(value ="tb_qq_auth")
+@Data
+public class QQAuth implements Serializable {
+    /**
+     * 账号id
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户id
+     */
+    private Integer userId;
+
+    /**
+     * 登录日志id
+     */
+    private Integer loginLogId;
+
+    /**
+     * QQ的openid
+     */
+    private String openid;
+
+    /**
+     * 访问QQ的token
+     */
+    private String accessToken;
+
+    /**
+     * 0未锁定，1已锁定，默认0
+     */
+    private Boolean lockedFlag;
+
+    /**
+     * 0未删除，1已删除，默认0
+     */
+    private Boolean deletedFlag;
+
+    /**
+     * 0未禁用，1已禁用，默认0
+     */
+    private Boolean disabledFlag;
+
+    /**
+     * 创建人
+     */
+    private Integer createUser;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新人，默认null
+     */
+    private Integer updateUser;
+
+    /**
+     * 更新时间，默认null
+     */
+    private Date updateTime;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
